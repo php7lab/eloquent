3f0d61de0cd4bdb781805aecf044db91f40cf31c
Vitaliy <theyamshikov@yandex.ru>
2019-11-09 14:24:48 +0600

    DI for config file

--- src/Fixture/Traits/ConfigTrait.php
+++ src/Fixture/Traits/ConfigTrait.php
@@ -10,7 +10,7 @@ trait ConfigTrait
     public function loadConfig($mainConfigFile = null)
     {
         if($mainConfigFile == null) {
-            $mainConfigFile = $_ENV['ELOQUENT_CONFIG_FILE'];
+            //$mainConfigFile = $_ENV['ELOQUENT_CONFIG_FILE'];
         }
         $config = include(__DIR__ . '/../../../../../../' . $mainConfigFile);
         return $config;
